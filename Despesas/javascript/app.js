5166b7ae8e2bb79137fad32fc4d81c6948beee87
DouglasFernandes <dfdsf1996@gmail.com>
2024-02-12 15:04:01 -0300

    Aula de javascript  12/02
    - Novos elemento de app.js

--- Despesas/javascript/app.js
+++ Despesas/javascript/app.js
@@ -40,6 +40,22 @@ class Bd {
 
 		localStorage.setItem('id', id)
 	}
+
+	recperarTodosRegistros() {
+		let despesas = Array()
+		let id = localStorage.getItem('id')
+
+		for (let  i = 1; i <= id; i++){
+			
+			let despesa = JSON.parse(localStorage.getItem(i))
+			
+			if( despesa === null){
+				continue
+			}
+
+			despesas.push(despesa)
+		}
+	}
 }
 
 let bd = new Bd()
@@ -85,4 +101,23 @@ function cadastrarDespesa() {
 		//dialog de erro
 		$('#modalRegistraDespesa').modal('show') 
 	}
+}
+
+function carregarListaDespesa(){
+	let despesas = Array()
+	
+	despesas = bd.recperarTodosRegistros()
+
+	let listaDespesa = document.getElementById('listaDespesa')
+
+
+	despesas.forEach(function(d){
+		let linha =  listaDespesa.insertRow()
+
+		linha.insertCell(0).innerHTML =  `${d.dia}/${d.mes}/${d.ano}`
+		linha.insertCell(1).innerHTML = d.tipo
+		linha.insertCell(2).innerHTML = d.descricao
+		linha.insertCell(3).innerHTML = d.tipo
+	})
+
 }
