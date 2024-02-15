48088e5978948d8deea08ac407696bae5f5dc648
DouglasFernandes <dfdsf1996@gmail.com>
2024-02-15 14:32:44 -0300

    Despesas Add / Remove
    - Removendo formação de arquivos antigos.
    - Add novo formação de arquivos de javascript

---
+++ Despesas/javascript/carregaListaDespesas.mjs
@@ -0,0 +1,47 @@
+function carregaListaDespesas() {
+
+	let despesas = Array()
+
+	despesas = bd.recuperarTodosRegistros() 
+
+	/*
+
+	<tr>
+		<td>15/03/2018</td>
+		<td>Alimentação</td>
+		<td>Compras do mês</td>
+		<td>444.75</td>
+	</tr>
+
+	*/
+
+	let listaDespesas = document.getElementById("listaDespesas")
+
+	despesas.forEach(function(d){
+
+		//Criando a linha (tr)
+		var linha = listaDespesas.insertRow();
+
+		//Criando as colunas (td)
+		linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}` 
+
+		//Ajustar o tipo
+		switch(d.tipo){
+			case '1': d.tipo = 'Alimentação'
+				break
+			case '2': d.tipo = 'Educação'
+				break
+			case '3': d.tipo = 'Lazer'
+				break
+			case '4': d.tipo = 'Saúde'
+				break
+			case '5': d.tipo = 'Transporte'
+				break
+			
+		}
+		linha.insertCell(1).innerHTML = d.tipo
+		linha.insertCell(2).innerHTML = d.descricao
+		linha.insertCell(3).innerHTML = d.valor
+		console.log(d)
+	})
+ }
