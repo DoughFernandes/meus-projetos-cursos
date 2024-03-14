0446046834cd8a0e8271885c8e7f87f4079e164b
Douglas Fernandes <dfdsf1996@gmail.com>
2024-03-14 11:32:07 -0300

    Fim do projeto Pokedex

--- pokedex/assets/javascript/poke-api.js
+++ pokedex/assets/javascript/poke-api.js
@@ -2,7 +2,7 @@ const pokeApi = {};
 
 function convertPokeApiDetailToPokemon(pokeDetail){
     const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.order
+    pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
 
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
@@ -24,8 +24,8 @@ pokeApi.getPokemonDetail = (pokemons) => {
 
 
 
-pokeApi.getPokemons = (offset, limit) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset =${offset}&limit=${limit}`;
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
     return fetch(url)
         .then((response) => response.json())
