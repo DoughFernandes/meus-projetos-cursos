a5c9a5e4960eeabd39045ed7adbc94bf0c0a2e00
Douglas Fernandes <dfdsf1996@gmail.com>
2024-07-02 12:00:36 -0400

    Mudança na Requisição API
    - Exclusão dos arquivos API data/db.json
    - Adicionando nova rota API, para uma requisição direto GitHub Respositorio Data/spider

--- Spider-verse/spider-next/next.config.mjs
+++ Spider-verse/spider-next/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: 'http://localhost:8000',
+    API_URL: 'https://raw.githubusercontent.com/DoughFernandes',
     DOMAIN_ORIGIN: 'http://localhost:8000'
   }
 };
